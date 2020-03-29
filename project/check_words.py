e23929ba32c2ce4d65f893160f7f49060ef6946f
griffin brome <griffinbrome@gmail.com>
2020-03-28 22:16:20 -0700

    check_words.py operates on paragraph level, but is broken :(

--- project/check_words.py
+++ project/check_words.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
 import re
 # source doc
-src = "../data/master/EXAMPLE-PLAGIARIZED-DOC.txt" 
+src = "data/master/EXAMPLE-PLAGIARIZED-DOC.txt" 
 # reference doc
-ref = "../data/1-1000.txt"
+ref = "data/1-1000.txt" 
 # remove all occurances of the n most common words
 idx = "100"
+# path to outwritten file
+out = "data/"
+# read common words into an array for later
 with open(ref, "r") as fin:
     comm_words=[line.strip("\n").lower() for line in fin]
-src_words = []
-with open(src, "r") as fin:
-    for line in fin:
-        for word in line.split():
-            word = word.lower()
-            if not word in comm_words:
-                src_words.append(word)
+# split on paragraphs
+delimiters = r"\n{2,}|={2,}|-{2,}"
+with open(src,"r") as fin:
+    contents = fin.read()
+    paragraphs = re.split(delimiters, contents)
+# remove common words from each paragraph
+for paragraph in paragraphs:
+    src_words = [word for word in paragraph.split(" ") 
+                 if not word.lower() in comm_words]
+    paragraph = " ".join(src_words)
+# reform into original structure
+with open(out + "cleaned.txt","w") as fout:
+    for paragraph in paragraphs:
+        fout.write(paragraph)
+        fout.write("\n\n")
